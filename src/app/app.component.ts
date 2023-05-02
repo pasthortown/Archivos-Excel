88063cfc54bb50a9cfad0ede66a38b0e82ef344d
Luis Alfonso Salazar Vaca <luis.salazar@kfc.com.ec>
2023-05-02 17:03:25 -0500

    inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { NgxFileDropEntry, FileSystemFileEntry } from 'ngx-file-drop';
+import { FileSaverService } from 'ngx-filesaver';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,78 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'xlsmergetool';
+  seleccion = 'Consolidado';
+  validate_file_size: boolean = false;
+  max_file_size: number = 10;
+  max_file_count: number = 50;
+  files: any[] = [];
+  accept: string = '*';
+
+  constructor(
+    private toastr: ToastrService,
+    private fileServerService: FileSaverService
+    ) { }
+
+  dropped(files: NgxFileDropEntry[]) {
+    for (const droppedFile of files) {
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        fileEntry.file((file: File) => {
+          const reader = new FileReader();
+          reader.readAsDataURL(file);
+          reader.onload = () => {
+            if (reader.result != null) {
+              let new_file = {
+                name: file.name,
+                type: file.type,
+                size: file.size,
+                file_base64: reader.result.toString().split(',')[1],
+              };
+              this.files.push(new_file);
+              this.validate_files();
+            }
+          };
+        });
+      }
+    }
+  }
+
+  validate_files() {
+    this.validate_file_size = true;
+    this.files.forEach((file: any) => {
+      if (file.size > (this.max_file_size * 1024 * 1024)) {
+        this.validate_file_size = false;
+      }
+    });
+  }
+
+  download_file(item: any) {
+    this.download(item);
+  }
+
+  download(item: any) {
+    const byteCharacters = atob(item.file_base64);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+       byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray], { type: item.type});
+    this.fileServerService.save(blob, item.name);
+  }
+
+  delete_file(file: any) {
+    let new_files: NgxFileDropEntry[] = [];
+    this.files.forEach(element => {
+      if (element != file) {
+        new_files.push(element);
+      }
+    });
+    this.files = new_files;
+    this.validate_files();
+  }
+
+  process_files(seleccion: string) {
+    console.log(seleccion);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FileSaverModule } from 'ngx-filesaver';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgxFileDropModule } from 'ngx-file-drop';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    NgbModule,
+    FileSaverModule,
+    ToastrModule.forRoot(),
+    NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' }),
+    NgxFileDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
